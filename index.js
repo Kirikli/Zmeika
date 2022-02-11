995178205e0544f3f32d6139a184dccc1e695baa
Kirill Lopyrin <lirikk2020@gmail.com>
2022-02-11 13:05:45 +0300

    Day2

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const appState = {
+  cells: [],
+};
+
 const onDOMIsReady = () => {
   console.log('!!!!!!');
   init();
 }
+
+// appState.cells.find((cell) => cell.rowIndex === 5 && cell.columnIndex === 2);
 
 const init = () => {
   // ... click on the button play
@@ -14,22 +20,52 @@ const onButtonPlayClicked = () => {
   //Ищем second-screen и убираем ему класс hidden
   document.querySelector("#second-screen").classList.remove('hidden');
 
-  
+  generateField();
+  generateSnake();
+
 }
 
+//const generateSnake = () => {
+  //for (let i = 0; i<5; i+= 1){
+ //   if(appState.cells.find((cell) => cell.rowIndex === 5 && cell.columnIndex === 2){}   
+ // }
+
+//}
 const generateField = () => {
   const gameField = document.querySelector("#game-field");
   for (let rowIndex = 0; rowIndex < 20; rowIndex += 1) {
     for (let columnIndex = 0; columnIndex < 20; columnIndex += 1) {
-      // создаем div
-      <div clas="cell">
-        gameField.classList.add('cell')
-      </div>
-      // добавляем ему класс cell
+      //создаем div
+      let div = document.createElement('div');
+      //добавляем ему класс cell
+      div.classList.add("cell");
       // вставляем в game field
+      gameField.appendChild(div);
+      const cell = {
+        rowIndex,
+        columnIndex,
+        element: div,
+      };
+      appState.cells.push(cell);
+
     }
   }
 }
+
+const generateSnake = () =>{
+  let a = appState.cells.find((cell) => cell.rowIndex === 5 && cell.columnIndex === 2);
+  a.classList.add('cellsnake')
+}
+// const generateSnake = () => {
+//   const gameSnake = document.querySelector("#snake");
+//   for (let rowIndex = 0; rowIndex < 1; rowIndex += 1) {
+//     for (let columnIndex = 0; columnIndex < 3; columnIndex += 1) {
+//       let div = document.createElement('div');
+//       div.classList.add("cellsnake");
+//       gameSnake.appendChild(div);
+//     }
+//   }
+// }
 
 const main = () => {
   if (document.readyState === 'complete') onDOMIsReady();
